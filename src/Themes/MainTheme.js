c51cc4338e7da6c94a425baebbff068a60d1e15d
Tasneem Sawalha <tasneem31997@gmail.com>
2025-12-24 19:36:05 +0200

    update main theme

--- src/Themes/MainTheme.js
+++ src/Themes/MainTheme.js
@@ -1,16 +1,33 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
- const MainTheme = createTheme({
+const MainTheme = createTheme({
   palette: {
     text: {
-      primary: "#2B3445",    
-     // secondary: "#777777", 
-     mainColor:"#E94560"  
+      primary: "#2B3445",
+      // secondary: "#777777", 
+      mainColor: "#E94560"
+    },
+    primary: {
+      main: "#E94560"
+    },
+    secondary: {
+      main: '#2B3445',
+    },
+    muted: {
+      main: '#7d879c',
+    },
+    customGray: {
+      main: '#f8f9fa',
+
+    },
+    background: {
+      default: '#f8f9fa',
+      paper: '#ffffff',
     },
   },
-    typography: {
-      fontFamily: '"Public Sans", "Public Sans Fallback", sans-serif',
-    },
-  });
+  typography: {
+    fontFamily: '"Public Sans", "Public Sans Fallback", sans-serif',
+  },
+});
 
 
-  export default MainTheme
+export default MainTheme
