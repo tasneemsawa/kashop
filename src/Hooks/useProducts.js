1f120c499142e90507a629bde220658d49e05053
Tasneem Sawalha <tasneem31997@gmail.com>
2025-12-27 16:52:59 +0200

    update use products

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -6,17 +6,18 @@ import {
 } from '@tanstack/react-query'
 import axiosInstance from '../API/AxiosInstance'
 
-export function useProducts() {
+export function useProducts(params) {
 
     const getProducts = async () => {
-        let response = await axiosInstance.get("/Products")
+        let response = await axiosInstance.get("/Products",{ params })
         console.log(response)
         return response.data.response.data
     }
     const query = useQuery({
-        queryKey: ['products'],
+        queryKey: ['products', params],
         staleTime: 300000,//every 5 min 
-        queryFn: getProducts
+        queryFn: getProducts,
+        keepPreviousData: true,
     })
 
 
