724ba6605f6713d1711713e5d607ca1546a84b11
Tasneem Sawalha <tasneem31997@gmail.com>
2026-01-02 20:19:09 +0200

    adding Features Cards

---
+++ src/pages/Home/FeaturesCards/FeaturesCards.jsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import { Box, Container, Grid, Typography, Card } from '@mui/material';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import PaymentOutlinedIcon from '@mui/icons-material/PaymentOutlined';
+import VerifiedUserOutlinedIcon from '@mui/icons-material/VerifiedUserOutlined';
+import HeadsetMicOutlinedIcon from '@mui/icons-material/HeadsetMicOutlined';
+import Styles from './Styles';
+
+const features = [
+    { icon: <LocalShippingOutlinedIcon fontSize="large" />, title: 'Worldwide Delivery' },
+    { icon: <PaymentOutlinedIcon fontSize="large" />, title: 'Safe Payment' },
+    { icon: <VerifiedUserOutlinedIcon fontSize="large" />, title: 'Shop With Confidence' },
+    { icon: <HeadsetMicOutlinedIcon fontSize="large" />, title: '24/7 Support' },
+];
+
+const FeaturesCards = () => {
+    return (
+        <Container maxWidth="lg" sx={{ py: 8 }}>
+            <Grid container spacing={3}>
+                {features.map((item, index) => (
+                    <Grid size={{ xs: 12, sm: 6, md: 3 }} key={index}>
+                        <Card sx={Styles.mainBox} elevation={0}>
+                            <Box sx={Styles.iconCircle}>
+                                {item.icon}
+                            </Box>
+                            <Typography
+                                sx={Styles.title}
+                            >
+                                {item.title}
+                            </Typography>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
+};
+
+export default FeaturesCards;
